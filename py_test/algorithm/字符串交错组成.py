3415e174483503fd0d286a33dd848e761747fa8d
feng <854591086@qq.com>
2019-01-22 23:09:34 +0800

    list

--- py_test/algorithm/字符串交错组成.py
+++ py_test/algorithm/字符串交错组成.py
@@ -1,7 +1,6 @@
 # 题目描述
 #
 # 对于三个字符串A，B，C。我们称C由A和B交错组成当且仅当C包含且仅包含A，B中所有字符，且对应的顺序不改变。请编写一个高效算法，判断C串是否由A和B交错组成。
-#
 # 给定三个字符串A,B和C，及他们的长度。请返回一个bool值，代表C是否由A和B交错组成。保证三个串的长度均小于等于100。
 # 测试样例：
 #
@@ -9,3 +8,14 @@
 #
 # 返回：true
 #
+# 我用了比较下流的方法： 将a,b字符串组合起来然后排序，c也排序然后比较排序后的结果相等的话就返回true
+class Mixture:
+    def chkMixture(self, A, n, B, m, C, v):
+        # write code here
+        if n+m<=100 and v<=100:
+            d = list(A+B)
+            d.sort()
+            e = list(C)
+            e.sort()
+            if d == e:
+                return True
---
+++ py_test/链表/单链表.py
@@ -0,0 +1,86 @@
+class Node(object):
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+class sigle_list(object):
+    """使用默认参数"""
+    def __init__(self,node = None):
+        """保存头节点 即第一个节点"""
+        self._head = node
+
+    """判断链表是否为空"""
+    def is_empty(self):
+        if self._head == None:
+            return True
+        else:
+            return False
+
+
+    def The_length(self):
+        """定义游标current进行遍历 cur指向头节点"""
+        cur = self._head
+        count = 0
+        while cur != None:
+            count += 1
+            cur = cur.next
+        return count
+
+    """遍历链表"""
+    def travel(self):
+        cur = self._head
+
+        while cur != None:
+            print(cur.value,end=" ")
+            cur = cur.next
+
+    """头插法"""
+    def add(self,value):
+        node = Node(value)
+        node.next = self._head
+        self._head = node
+
+    def insert(self,value,index):
+        if index <= 0:
+            self.add(value)
+        elif index >= self.The_length()-1:
+            self.append(value)
+        else:
+            node = Node(value)
+            count = 0
+            pre = self._head
+            while count != index-1:
+                count += 1
+                pre = pre.next
+            node.next = pre.next
+            pre.next = node
+
+
+
+    """尾插法"""
+    def append(self,value):
+        """将传入的值构造为一个节点"""
+        node = Node(value)
+        if self.is_empty():
+            self._head = node
+        else:
+            cur = self._head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+
+
+    """删除节点"""
+    def remove(self):
+        pass
+
+if __name__ == '__main__':
+    li = sigle_list()
+    print(li.is_empty())
+    li.append(1)
+    li.append(2)
+    li.append(3)
+    li.insert(9,3)
+    print(li.is_empty())
+    print(li.The_length())
+    li.travel()
+
